c62a5fda31e282b31702b42fc3a4b243650988bf
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2022-07-25 12:07:02 +0400

    changes

--- Resume Web Application/src/main/java/Resume_Desktop_App/JDBC/dao/EmploymentHistoryInterface.java
+++
@@ -1,9 +0,0 @@
-package Resume_Desktop_App.JDBC.dao;
-import Resume_Desktop_App.JDBC.entity.EmploymentHistory;
-
-
-import java.util.List;
-
-public interface EmploymentHistoryInterface {
-    public List<EmploymentHistory> getAllEmploymentHistory(int id);
-}
--- Resume Web Application/src/main/java/Resume_Desktop_App/JDBC/dao/NationalityInterface.java
+++
@@ -1,9 +0,0 @@
-package Resume_Desktop_App.JDBC.dao;
-import Resume_Desktop_App.JDBC.entity.Nationality;
-
-
-import java.util.List;
-
-public interface NationalityInterface {
-    public List<Nationality> getAllNationalities();
-}
